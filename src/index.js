ccc9478ca09547378a1b3f49ead6f6d95c02e6cb
timmy277 <botono123@gmail.com>
2025-01-13 19:22:03 +0700

    fix path

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 3001;
 
-app.use("/swagger", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use("./swagger", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use(cors());
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb" }));
