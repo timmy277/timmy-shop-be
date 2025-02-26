18666fd1eb464a0733fe78c05a6d95adc8120ae5
timmy277 <botono123@gmail.com>
2025-02-26 20:01:27 +0700

    alter order model

--- src/models/OrderProduct.js
+++ src/models/OrderProduct.js
@@ -5,8 +5,9 @@ const orderSchema = new mongoose.Schema(
     orderItems: [
       {
         name: { type: String, required: true },
+        slug: { type: String, required: true },
         amount: { type: Number, required: true },
-        // image: { type: String, required: true },
+        image: { type: String, required: true },
         price: { type: Number, required: true },
         discount: { type: Number },
         product: {
